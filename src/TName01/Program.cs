24b0bc1dae4aa250cf1833b856311426a7e2ccf5
kiran.gondi <kiran.gondi@planview.com>
2024-08-16 16:20:53 +0530

    initial cut

---
+++ src/TName01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string input = "OLANZAPINE 10 MG - ORODISPERSIBLE TABLET";
+string pattern = @"\b[A-Z]+\b(?:\s+\d+\s+[A-Z]+)?(?:\s+-\s+[A-Z]+)?";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(input);
+
+foreach (Match match in matches)
+{
+    Console.WriteLine(match.Value);
+}
+Console.ReadKey();
